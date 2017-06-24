dc519522f8ca2244935096fcf5d4c9e3d838651c
Artur Alves <arturhgca@gmail.com>
2017-06-24 19:48:10 -0300

    Continued remake

--- ytdldotnet/EasyInterfacePage.xaml.cs
+++ ytdldotnet/EasyInterfacePage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,70 @@ namespace ytdldotnet
     /// <summary>
     /// Interaction logic for EasyInterfacePage.xaml
     /// </summary>
-    public partial class EasyInterfacePage : Page
+    public partial class EasyInterfacePage : Page, INotifyPropertyChanged
     {
+        // data bindings
+        string url;
+        string statusText;
+        string localPath;
+
+        // control bindings
+        bool isStatusTextVisible;
+        bool isConversionFormatVisible;
+        bool canConvert;
+        bool isLocalPathBoxVisible;
+
         public EasyInterfacePage()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            IsStatusTextVisible = false;
+            IsConversionFormatVisible = false;
+            CanConvert = Properties.Settings.Default.CanConvert;
+            IsLocalPathBoxVisible = false;
         }
+
+        // PROPERTIES
+
+        private void NotifyPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Url { get => url; set => url = value; }
+        public string StatusText { get => statusText; set => statusText = value; }
+        public string LocalPath { get => localPath; set => localPath = value; }
+        public bool IsStatusTextVisible
+        {
+            get => isStatusTextVisible;
+            set
+            {
+                isStatusTextVisible = value;
+                NotifyPropertyChanged("IsStatusTextVisible");
+            }
+        }
+        public bool IsConversionFormatVisible
+        {
+            get => isConversionFormatVisible;
+            set
+            {
+                isConversionFormatVisible = value;
+                NotifyPropertyChanged("IsConversionFormatVisible");
+            }
+        }
+        public bool CanConvert { get => canConvert; set => canConvert = value; }
+        public bool IsLocalPathBoxVisible
+        {
+            get => isLocalPathBoxVisible;
+            set
+            {
+                isLocalPathBoxVisible = value;
+                NotifyPropertyChanged("IsLocalPathBoxVisible");
+            }
+        }
+
     }
 }
